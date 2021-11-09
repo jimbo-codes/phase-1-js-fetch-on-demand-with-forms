7fce45974ea2d24c55999634ab8f3593db18b8b3
James Burns <jrburns96@aol.com>
2021-11-09 11:27:45 -0500

    working live fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const input = inputForm.searchByID.value;
+        fetch(`http://localhost:3000/movies/${input}`)
+        .then((resp) => resp.json())
+        .then(data => {
+            const title = document.querySelector('h4');
+            const summary = document.querySelector('p');
+            title.textContent = data.title;
+            summary.textContent = data.summary;
+        }),
+        inputForm.reset();
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
